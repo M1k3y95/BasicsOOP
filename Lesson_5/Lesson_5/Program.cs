ba598f65454d07ecf327a69d45274291d7089bbf
M1k3y95 <rockheart95@gmail.com>
2021-12-08 20:28:24 +0500

    Homework lesson-5 - operators overloading

---
+++ Lesson_5/Lesson_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational_Number rNumber1 = new(2, 3);
+            Rational_Number rNumber2 = new(3, 5);
+
+            Rational_Number rNumber3 = rNumber1 * rNumber2;
+            Console.WriteLine(rNumber3.ToString());
+
+            rNumber3 = rNumber1 / rNumber2;
+            Console.WriteLine(rNumber3.ToString());
+
+            rNumber3 = rNumber1 + rNumber2;
+            Console.WriteLine(rNumber3.ToString());
+
+            rNumber3++;
+            Console.WriteLine(rNumber3.ToString());
+
+            float something = (float)rNumber3;
+            Console.WriteLine(something);
+
+            int somethingtoo = (int)rNumber2;
+            Console.WriteLine(somethingtoo);
+        }
+    }
+}
